aa00d0b083392bbc692a529a5c699515cd14414a
Alex-Joe <alex.ribeiro.souza@gmail.com>
2020-11-11 09:35:10 -0300

    Criado as funções de movimento do jogador

--- js/js.js
+++ js/js.js
@@ -10,6 +10,24 @@ function start() { // Inicio da função start()
 //Principais variáveis do jogo
 	
 	var jogo = {}
+	var TECLA = {
+	W: 87,
+	S: 83,
+	D: 68
+	}
+
+	jogo.pressionou = [];
+	
+	//Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+	jogo.pressionou[e.which] = true;
+	});
+
+
+	$(document).keyup(function(e){
+       jogo.pressionou[e.which] = false;
+	});
 	
 	//Game Loop
 
@@ -17,18 +35,47 @@ function start() { // Inicio da função start()
 	
 	function loop() {
 	
-	movefundo();
+	moveFundo();
+	moveJogador();
 	
 	} // Fim da função loop()
 	
 	//Função que movimenta o fundo do jogo
 	
-	function movefundo() {
+	function moveFundo() {
 	
 	esquerda = parseInt($("#fundoGame").css("background-position"));
 	$("#fundoGame").css("background-position",esquerda-1);
 	
-	} // fim da função movefundo()
+	} // fim da função moveFundo()
 
+
+	function moveJogador() {
+	
+	if (jogo.pressionou[TECLA.W]) {
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo-10);
+	    
+		if (topo<=0) {
+		$("#jogador").css("top",topo+10);
+        }
+    }
+	
+	if (jogo.pressionou[TECLA.S]) {
+		
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo+10);
+
+        if (topo>=434) {	
+	    $("#jogador").css("top",topo-10);
+		}		
+	}
+	
+	//if (jogo.pressionou[TECLA.D]) {
+		
+		//Chama função Disparo	
+	//}
+
+	} // fim da função moveJogador()
 
 } // Fim da função start
